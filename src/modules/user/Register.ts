d13676b325ce4d7a449a63f5187071f7a77b3fc9
yuuta <yuuta_cookie@icloud.com>
2020-01-12 02:00:11 +0900

    インプットタイプを適用

--- src/modules/user/Register.ts
+++ src/modules/user/Register.ts
@@ -1,19 +1,20 @@
 import { Resolver, Query, Mutation, Arg } from 'type-graphql';
-import * as bcrypt from 'bcryptjs';
+import bcrypt from 'bcryptjs';
 import { User } from '../../entity/User';
-// スキーマ
-@Resolver(User)
+import { RegisterInput } from './register/RegistarInput';
+// 登録スキーマ
+@Resolver()
 export class RegisterResolver {
+  // Hello,Worldするためのクエリ
   @Query(() => String)
   async hello() {
     return 'Hello,World';
-  } // MutationはRESTfullでいうとcreate,update,deleteにあたる
+  }
+  // MutationはRESTfullでいうとcreate,update,deleteにあたる
   @Mutation(() => User)
-  // 非同期処理でユーザーを登録するregister関数を作成
+  // 非同期処理でユーザーを登録するregisterメソッドを作成、入力タイプはRegisterInputから受け取ってる
   async register(
-    @Arg('userName') userName: string,
-    @Arg('email') email: string,
-    @Arg('password') password: string
+    @Arg('data') { userName, email, password }: RegisterInput
   ): Promise<User> {
     // ハッシュ関数を生成
     const hashedPassword = await bcrypt.hash(password, 12);
